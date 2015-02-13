2150cd43cb13b618fa90b01b656c25d156ea1614
polamjag <s@polamjag.info>
2015-02-13 15:04:09 +0900

    initial

---
+++ lib/traktor/nml.rb
@@ -0,0 +1,7 @@
+require "traktor/nml/version"
+
+module Traktor
+  module Nml
+    # Your code goes here...
+  end
+end
---
+++ lib/traktor/nml/version.rb
@@ -0,0 +1,5 @@
+module Traktor
+  module Nml
+    VERSION = "0.0.1"
+  end
+end
